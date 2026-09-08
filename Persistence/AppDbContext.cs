1e2556af9b2e1c24f70beac027157e3638768e7b
Edris <Edris@EDRIS-MIS>
2026-09-08 21:53:30 +0430

    add db context and configurations

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
