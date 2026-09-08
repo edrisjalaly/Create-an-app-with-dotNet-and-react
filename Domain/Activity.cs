46becce20e5fe23bb8af0957f959a673107e4e90
Edris <Edris@EDRIS-MIS>
2026-09-08 21:22:34 +0430

    add first modal

---
+++ Domain/Activity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public string Id {  get; set; } = Guid.NewGuid().ToString();
+        public string? Title { get; set; }
+        public DateTime Date { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public bool isCancelled { get; set; }
+        
+
+        public string? City { get; set; }
+        public string? Venue { get; set; }
+
+        public double Latitude {  get; set; }
+        public double Longitude { get; set; }
+    }
+}
