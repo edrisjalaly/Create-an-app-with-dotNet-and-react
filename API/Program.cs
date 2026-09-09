5569842464b7e0f924acfcd2c72250ec485012c7
Edris <Edris@EDRIS-MIS>
2026-09-09 20:23:08 +0430

    add first modal instance to database

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.MapControllers();
 
