e141fd5a2ceb2f8878c36253d47ed6a5d3c6e9b5
Edris <Edris@EDRIS-MIS>
2026-09-09 20:50:23 +0430

    add seeding data and aumomatic seeding when start program

--- API/Program.cs
+++ API/Program.cs
@@ -27,4 +27,19 @@ if (app.Environment.IsDevelopment())
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbSeeder.SeedDb(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "this is the error");
+}
+
 app.Run();
---
+++ Persistence/DbSeeder.cs
@@ -0,0 +1,47 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DbSeeder
+    {
+        public static async Task SeedDb(AppDbContext context)
+        {
+            if (context.Activities.Any()) return;
+
+            var activities = new List<Activity>
+            {
+               new()
+               {
+                   Title = "run",
+                   Date = DateTime.Now,
+                   Description = "run from now ",
+                   Category = "sport",
+                   City = "kabul",
+                   Venue = "markaz",
+                   Latitude = 90,
+                   Longitude = 80,
+               },
+               new()
+               {
+                   Title = "swim",
+                   Date = DateTime.Now,
+                   Description = "swim from now ",
+                   Category = "sport",
+                   City = "kabul",
+                   Venue = "markaz",
+                   Latitude = 90,
+                   Longitude = 80,
+               }               
+            };
+
+
+           context.Activities.AddRange(activities);
+           await context.SaveChangesAsync();
+        }
+    }
+}
