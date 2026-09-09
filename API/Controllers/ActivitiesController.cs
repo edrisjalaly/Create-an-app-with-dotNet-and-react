3bf04cdf93cfbf7eaf3ee53603cd78687def5bd4
Edris <Edris@EDRIS-MIS>
2026-09-09 21:26:25 +0430

    add first controller

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseController
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ActivitiesController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _dbContext.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+        {
+            var activity = await _dbContext.Activities.FindAsync(id);
+            if (activity == null) return NotFound();
+
+            return activity;
+        }
+    }
+}
