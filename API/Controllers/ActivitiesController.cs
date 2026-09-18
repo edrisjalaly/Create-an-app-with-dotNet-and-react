de2b79a454a6bfb90e2a8d6c5c226c3bc458d281
Edris <Edris@EDRIS-MIS>
2026-09-18 19:56:14 +0430

    make the controller thinner

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -9,23 +9,17 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
 
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new GetActiviesList.Query());
+            return await Mediator.Send(new GetActiviesList.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivityDetail(string id)
         {
-            return await _mediator.Send(new GetActivityDetails.Query{ Id = id} );
+            return await Mediator.Send(new GetActivityDetails.Query{ Id = id} );
         }
     }
 }
